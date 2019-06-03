5e8b6096b3e311034cb7ca20a1b9025488857f52
Huong <huongnguyenak96@gmail.com>
2019-06-03 19:57:57 +0700

    class

---
+++ a.js
@@ -0,0 +1,18 @@
+// let teo = {
+//     name : 'Teo',
+//     age: 11
+// }
+
+// let ti = {
+//     name : 'Ti',
+//     age: 12
+// }
+
+class Person {
+    constructor(txtName){
+        this.name = txtName;
+    }
+}
+const teo = new Person('Teo');
+// teo.name = 'Teo';
+console.log(teo);
